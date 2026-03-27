1a127cbc4a0d7ac0048f8c7fb57e0aab98f13c40
Salman Sattar <salmansattar24@gmail.com>
2026-03-27 16:31:40 -0700

    Export config dictionaries from config/__init__.py
    - Import all 8 config dicts from config.py
    - Now works: from config import noise_agent_config

--- config/__init__.py
+++ config/__init__.py
@@ -3,3 +3,26 @@ Configuration module
 
 This package contains configuration dictionaries for all agents and the simulation environment.
 """
+
+# Import all configuration dictionaries
+from .config import (
+    noise_agent_config,
+    sl_agent_config,
+    linear_sl_agent_config,
+    market_agent_config,
+    rl_agent_config,
+    strategic_agent_config,
+    initial_agent_config,
+    observation_agent_config,
+)
+
+__all__ = [
+    "noise_agent_config",
+    "sl_agent_config",
+    "linear_sl_agent_config",
+    "market_agent_config",
+    "rl_agent_config",
+    "strategic_agent_config",
+    "initial_agent_config",
+    "observation_agent_config",
+]
